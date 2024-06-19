536938f599a4652ed2717156094b41266512a271
rozzy <rosekaremeri@gmail.com>
2024-06-19 22:28:58 +0300

    home page user interface

--- front_end/src/pages/home.js
+++ front_end/src/pages/home.js
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import React from "react";
 
 const savingHome = () => {
@@ -7,17 +7,40 @@ const savingHome = () => {
       sx={{
         background:
           "linear-gradient(0deg, #022341 0%, #0F5074 50%, #042C43 100%)",
-        height: "1000vh",
+        height: "100vh",
       }}
     >
-      <Box
-      container
-      >
-<Box>
-    <TouchableOpacity>
-        Connect Wallet
-    </TouchableOpacity>
-</Box>
+      <Box container>
+        <Box  sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "flex-end",
+          p: "1rem",
+        }}>
+          <Button
+            sx={{
+              p: "10px",
+              backgroundColor: "#0681bd",
+              color: "#fff",
+              borderRadius: "5px",
+              fontSize: "10px",
+              "&:hover": {
+                backgroundColor:   "#056b9a",
+              },
+            }}
+          >
+            Connect Wallet
+          </Button>
+        </Box>
+        <Box
+        sx={{
+            display:"flex",
+            justifyContent:"space-between",
+            alignItems:"center"
+        }}>
+
+        </Box>
+
       </Box>
     </Box>
   );
